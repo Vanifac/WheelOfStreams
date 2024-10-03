353e502b2d9bbb8f7532f3087e99a3ad45402857
William <whbass99@gmail.com>
2024-10-02 15:51:57 -1000

    Build 1

--- WheelOfNames.py
+++ WheelOfNames.py
@@ -1,23 +1,34 @@
-import http.config
+import http.client
+import json
 
 # https://wheelofnames.stoplight.io/docs/wheelofnames/ck5d76uhtsg3s-update-a-wheel
 
 conn = http.client.HTTPSConnection("wheelofnames.com")
 
-payload = "{\n  \"config\": {\n    \"displayWinnerDialog\": true,\n    \"slowSpin\": false,\n    \"pageBackgroundColor\": \"#FFFFFF\",\n    \"description\": \"string\",\n    \"animateWinner\": false,\n    \"winnerMessage\": \"string\",\n    \"title\": \"string\",\n    \"type\": \"color\",\n    \"autoRemoveWinner\": true,\n    \"path\": \"string\",\n    \"customPictureName\": \"string\",\n    \"customCoverImageDataUri\": \"string\",\n    \"playClickWhenWinnerRemoved\": false,\n    \"duringSpinSound\": \"ticking-sound\",\n    \"maxNames\": 1000,\n    \"centerText\": \"string\",\n    \"afterSpinSoundVolume\": 50,\n    \"spinTime\": 10,\n    \"hubSize\": \"S\",\n    \"coverImageName\": \"string\",\n    \"entries\": [\n      {\n        \"text\": \"string\",\n        \"image\": \"string\",\n        \"color\": \"string\",\n        \"weight\": 1,\n        \"id\": \"string\",\n        \"enabled\": true,\n        \"sound\": \"string\",\n        \"message\": \"string\"\n      }\n    ],\n    \"isAdvanced\": true,\n    \"galleryPicture\": \"string\",\n    \"customPictureDataUri\": \"string\",\n    \"showTitle\": true,\n    \"displayHideButton\": true,\n    \"afterSpinSound\": \"string\",\n    \"colorSettings\": [\n      {\n        \"color\": \"#FFFFFF\",\n        \"enabled\": true\n      }\n    ],\n    \"duringSpinSoundVolume\": 50,\n    \"displayRemoveButton\": true,\n    \"pictureType\": \"none\",\n    \"allowDuplicates\": true,\n    \"coverImageType\": \"\",\n    \"drawOutlines\": false,\n    \"launchConfetti\": true,\n    \"drawShadow\": true\n  }\n}"
 
-headers = {
-    'x-api-key': "",
-    'Content-Type': "application/json",
-    'Accept': "application/json, application/xml"
-}
+def get_wheels(api_key: str):
+    headers = {
+        'x-api-key': api_key,
+        'Accept': "application/json"}
 
+    conn.request("GET", "/api/v1/wheels/private", headers=headers)
 
-conn.request("PUT", "/api/v1/wheels/private", payload, headers)
+    res = conn.getresponse()
+    data = json.loads(res.read().decode("utf-8"))
 
-
-res = conn.getresponse()
-data = res.read()
+    return data
 
 
-print(data.decode("utf-8"))
+def send_wheel(api_key: str, wheel: dict):
+    payload = json.dumps(wheel)
+    headers = {
+        'x-api-key': api_key,
+        'Content-Type': "application/json",
+        'Accept': "application/json, application/xml"}
+
+    conn.request("PUT", "/api/v1/wheels/private", payload, headers)
+
+    res = conn.getresponse()
+    data = res.read()
+
+    print(data.decode("utf-8"))
--- WheelOfStreams.py
+++ WheelOfStreams.py
@@ -1,20 +1,31 @@
-# import http.client
 import argparse
-# import config
-# import WheelOfNames
+import WheelOfNames
+# import json
+import logging
+import sys
+import time
 
 # https://docs.python.org/3/library/argparse.html#the-add-argument-method
 # https://docs.streamer.bot/api/sub-actions/core/system/run-a-program
 
 
-testing = True
+testing = False
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='WheelOfStreams.log', encoding='utf-8', level=logging.INFO)
 
 
 def build_args():
     test_args = None
     if testing:
         print("Testing")
-        test_args = "-key 1342 -wheel Wheel -add vanifac".split(" ")
+        logging.info("Starting Test")
+        test_args = "-key baf23025-e96b-4feb-a3d5-ca807746adfb -wheel Wheel -add Vanifac".split(" ")
+        logging.info(test_args)
+        # test_args = "-key baf23025-e96b-4feb-a3d5-ca807746adfb -wheel Wheel -clear".split(" ")
+    else:
+        logging.info("Starting")
+        logging.info(sys.argv)
 
     parser = argparse.ArgumentParser(description="Configures WheelOfNames")
 
@@ -23,17 +34,64 @@ def build_args():
 
     action_group = parser.add_mutually_exclusive_group()
     action_group.add_argument('-add', nargs=1)
-    action_group.add_argument('-clear')
-
+    action_group.add_argument('-color', nargs=1)
+    action_group.add_argument('-clear', action='store_true')
     return parser.parse_args(test_args)
+
+
+def get_wheel_by_name(data: list, wheel_name: str):
+    for wheel in data:
+        if wheel['config']['title'] == wheel_name:
+            logging.info(f"Found wheel: {wheel_name}")
+            return wheel
+    return None
+
+
+def add_entry(wheel: dict, name: str):
+    print("Adding entry to", name)
+    logging.info(f"Adding entry to {name}")
+    i = 0
+    entry_found = False
+    for entry in wheel['config']['entries']:
+        if entry['text'] == name:
+            wheel['config']['entries'][i]['weight'] += 1
+            wheel['config']['entries'][i]['enabled'] = True
+            entry_found = True
+            break
+        i += 1
+    if not entry_found:
+        print("Entry not Found")
+        new_entry = {
+            "text": name,
+            "weight": 1,
+            "enabled": True}
+        wheel['config']['entries'].append(new_entry)
+    return
+
+
+def clear_entrys(wheel: dict):
+    for entry in wheel['config']['entries']:
+        entry['weight'] = 0
+        entry['enabled'] = False
 
 
 def run():
     args = build_args()
+    wheel_data = WheelOfNames.get_wheels(args.key[0])
+
+    wheel = get_wheel_by_name(wheel_data['data']['wheels'], args.wheel[0])
+    if not wheel:
+        print('wheel not found')
+        logging.error('Wheel by that name not found')
+        return
+
     if args.add:
-        print("adding entry to", args.add[0])
+        add_entry(wheel, args.add[0])
+    elif args.color:
+        print("setting color")
     elif args.clear:
-        print("clearing wheel")
+        clear_entrys(wheel)
+    WheelOfNames.send_wheel(args.key[0], wheel)
 
 
 run()
